9ea4e4b4e5a8c21d9f49295cafb9f3b22bda135e
dkwonkim <dkim10@bu.edu>
2017-02-27 13:34:58 -0500

    Add files via upload
    Working version
    Still need to add a line that send a whole dictionary to the central hub

--- ArduCAM_Testing/ArduCAM_Testing.ino
+++ ArduCAM_Testing/ArduCAM_Testing.ino
@@ -1,8 +1,10 @@
+#include <SoftwareSerial.h>
 #include <UTFT_SPI.h>
 #include <Wire.h>
 #include <ArduCAM.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Base64.h>
 #include "memorysaver.h"
 #include "ov5642_regs.h"
 
@@ -12,7 +14,7 @@ const int SPI_CS = 10;
 
 ArduCAM myCAM(OV5642,10);
 UTFT myGLCD(SPI_CS);
-
+SoftwareSerial bluetooth(2,3); //bluetooth connection
 void setup()
 {
   uint8_t vid,pid;
@@ -23,8 +25,13 @@ void setup()
   #if defined(__arm__)
     Wire1.begin(); 
   #endif
-  Serial.begin(57600);
-  Serial.println("ArduCAM Start!"); 
+//  bluetooth.begin(115200);
+  bluetooth.begin(9600);
+//  Serial.begin(115200);
+  delay(200);
+//  bluetooth.println("ArduCAM Start!"); 
+//  Serial.println("ArduCAM Start!"); 
+  delay(1000);
 
   // set the SPI_CS as an output:
   pinMode(SPI_CS, OUTPUT);
@@ -36,7 +43,8 @@ void setup()
   temp = myCAM.read_reg(ARDUCHIP_TEST1);
   if(temp != 0x55)
   {
-    Serial.println("SPI interface Error!");
+//    bluetooth.println("SPI interface Error!");
+//    Serial.println("SPI interface Error!");
     while(1);
   }
   
@@ -47,13 +55,16 @@ void setup()
   myCAM.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
   myCAM.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
   if((vid != 0x56) || (pid != 0x42))
+//    bluetooth.println("Can't find OV5642 module!");
     Serial.println("Can't find OV5642 module!");
   else
+//    bluetooth.println("OV5642 detected");
     Serial.println("OV5642 detected");
   
   //Change to JPEG capture mode and initialize the OV5642 module  
   myCAM.set_format(JPEG);
   myCAM.InitCAM();
+  myCAM.OV5642_set_JPEG_size(OV5642_640x480);
   myCAM.write_reg(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);    //VSYNC is active HIGH
 }
 
@@ -61,18 +72,14 @@ void loop()
 {
   uint8_t temp,temp_last;
   uint8_t start_capture = 0;
-
-  temp = Serial.read();
-  switch(temp)
-  {
-    case 0x10:
-      start_capture = 1;
-      Serial.println("Start Capture");  
+  char input = bluetooth.read(); // read from the bluetooth input
+  if(input == 'a'){
+    start_capture = 1;
+//      bluetooth.println("Start Capture");  
+//      Serial.println("Start Capture");  
       myCAM.flush_fifo();
-      break;
-    default:
-      break;
   }
+  delay(1000);
   //start_capture = 1;
   if(start_capture)
   {
@@ -83,15 +90,22 @@ void loop()
   }
   if(myCAM.get_bit(ARDUCHIP_TRIG,CAP_DONE_MASK))
   {
-    Serial.println("Capture Done!");
+//    bluetooth.println("Capture Done!");
+   
+    int inputLen = 0;
     
     while( (temp != 0xD9) | (temp_last != 0xFF) )
     {
         temp_last = temp;
         temp = myCAM.read_fifo();
-        Serial.write(temp);
+        bluetooth.write(temp);
+//        Serial.write(temp);
     }
-
+//    Serial.println("Capture Done!");
+    delay(2000);
+    bluetooth.println("done");   
+    // send the battery info
+    bluetooth.println("30");
     //Clear the capture done flag 
     myCAM.clear_fifo_flag();
     start_capture = 0;
